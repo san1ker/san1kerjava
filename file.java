59bbb757fb79ebcd4f0292030ecfd0297023822d
san1ker <san1ker@naver.com>
2022-05-24 16:32:03 +0900

    Create file.java

---
+++ file.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Main{
+    public static void main(String[] args) {
+
+        //scanner
+        Scanner scanner = null;
+        try{
+            scanner = new Scanner(new File("./tabular/qwe.txt"));   
+            scanner.useDelimiter("\n");
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+              
+        while (scanner.hasNext()) {            
+            String str = scanner.next();            
+            System.out.println(str);        
+        }
+        scanner.close();
+
+        //split
+        String str = "a,b,c,d,e,f,g,h";
+        String[] strArr = str.split(",");
+
+        for (int i=0; i<strArr.length;i++){
+            System.out.println(strArr[i]);
+        }
+
+        //strToInt
+        String a = "123";
+        int aa = Integer.parseInt(a);
+        String aaa = Integer.toString(aa);
+        System.out.println(aaa);
+    }   
+}
